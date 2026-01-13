9c41f953862e6d975fc499a32b2796570f429608
Victor <victormartinezrc@gmail.com>
2026-01-12 22:55:30 -0300

    updates in header, home and font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 ﻿import { products } from "../data/products";
 import ProductCard from "../components/ProductCard";
 import bannerVideo from "../assets/banner.mp4";
-import femaleImg from "../assets/female.jpg";
-import maleImg from "../assets/male.jpg";
+import femaleImg from "../assets/mujeres.jpg";
+import maleImg from "../assets/hombres.jpg";
 import "./Home.css";
 
 export default function Home() {
@@ -20,19 +20,26 @@ export default function Home() {
           <source src={bannerVideo} type="video/mp4" />
         </video>
         <div className="hero-content">
-          <h2>Windbreakers Aesthetic</h2>
+          <h2>
+            <span className="hero-line-1">¡Te acompañamos en tu </span>
+            <span className="hero-line-2">próxima aventura!</span>
+          </h2>
           <button onClick={scrollToProducts}><span>Ver colección</span></button>
         </div>
       </section>
 
       <section className="gender">
-        <div className="circle">
-          <img src={femaleImg} alt="Femenino" />
-          <span>Femenino</span>
+        <div className="gender-card">
+          <img src={maleImg} alt="Hombres" />
+          <div className="gender-overlay">
+            <h3>Hombres</h3>
+          </div>
         </div>
-        <div className="circle">
-          <img src={maleImg} alt="Masculino" />
-          <span>Masculino</span>
+        <div className="gender-card">
+          <img src={femaleImg} alt="Mujeres" />
+          <div className="gender-overlay">
+            <h3>Mujeres</h3>
+          </div>
         </div>
       </section>
 
